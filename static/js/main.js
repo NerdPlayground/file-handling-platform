4c3903da92ab624c63c9fb8cf8c7698f0c13c002
Nerd <studytime023@gmail.com>
2023-09-13 20:54:45 +0300

    chore (Project) : cleanup and add comments

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,14 @@
-let FILES={};
-let FILECOUNT=0;
-let FILESLOCKED=false;
+let FILES={}; // holds all staged files
+let FILECOUNT=0; // holds number of staged files
+let FILESLOCKED=false; // controls access to FILES in user operations
+
 let TRASH=document.getElementById("message").lastElementChild;
 let MESSAGE=document.getElementById("message").firstElementChild;
 
+/**
+ * informs user of the current stage state
+ * enables user to clear the stage
+*/
 function displayMessage(){
     if(FILECOUNT===0){
         TRASH.style.visibility="hidden";
@@ -11,10 +16,14 @@ function displayMessage(){
     }
     else{
         TRASH.style.visibility="visible";
-        return `${FILECOUNT} file${FILECOUNT>1?'s':''} staged for upload`;
+        return `${FILECOUNT} file${FILECOUNT>1?'s':''} staged for processing`;
     }
 }
 
+/**
+ * handles removing staged file from the DOM 
+ * handles removing file from the FILES object
+*/
 function remove(stagedFile){
     let fileName=stagedFile.firstElementChild.innerText;
     stagedFile.style.left="100%";
@@ -25,6 +34,9 @@ function remove(stagedFile){
     delete FILES[fileName]; FILECOUNT--;
 }
 
+/**
+ * removes all files from the stage
+ */
 function removeFilesFromStage(){
     if(FILESLOCKED) return;
     FILESLOCKED=true;
@@ -35,6 +47,9 @@ function removeFilesFromStage(){
     setTimeout(()=>FILESLOCKED=false,1000);
 }
 
+/**
+ * removes user selected file from the stage
+ */
 function removeFileFromStage(){
     if(FILESLOCKED) return;
     FILESLOCKED=true;
@@ -43,6 +58,9 @@ function removeFileFromStage(){
     setTimeout(()=>FILESLOCKED=false,1000);
 }
 
+/**
+ * adds selected file(s) to the stage
+ */
 function addFilesToStage(event){
     if(FILESLOCKED) return;
     FILESLOCKED=true;
@@ -53,24 +71,34 @@ function addFilesToStage(event){
     setTimeout(()=>FILESLOCKED=false,1000);
 }
 
+/**
+ * adds staged file to the DOM
+ * adds file to the FILES object
+ */
 function stageFiles(files){
-    // <div class="staged-file">
-    //     <span class="file-name">file-name.svg</span>
-    //     <span class="remove-file" onclick=removeFile(event)>X</span>
-    // </div>
+    // Staged file entity structure
+    /**
+     * <div class="staged-file">
+     *     <span class="file-name">file-name.svg</span>
+     *     <span class="remove-file" onclick=removeFile(event)>X</span>
+     * </div>
+    */
 
     let stageContents=document.getElementById("stage-contents");
     files.forEach(file=>{
         if(!(file["name"] in FILES)){
+            // <span class="file-name">file-name.svg</span>
             let fileName=document.createElement("span");
             fileName.innerText=file["name"];
             fileName.classList.add("file-name");
     
+            // <span class="remove-file" onclick=removeFile(event)>X</span>
             let removeFile=document.createElement("span");
             removeFile.innerText="X";
             removeFile.classList.add("remove-file");
             removeFile.addEventListener("click",removeFileFromStage);
     
+            // <div class="staged-file">
             let stagedFile=document.createElement("div");
             stagedFile.appendChild(fileName);
             stagedFile.appendChild(removeFile);
