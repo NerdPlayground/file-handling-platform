da9538b37481a9de66bddfc35dc516a18388386c
Nerd <studytime023@gmail.com>
2023-09-13 18:27:25 +0300

    chore (Project) : uninstall imageKit

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,3 @@
-var imageKit= new ImageKit({
-    publicKey: "public_e1TZPeVWcR0Pf8s2sWWRc1gqPN4=",
-    urlEndpoint: "https://ik.imagekit.io/uxrynlkmb",
-});
-
 let FILES={};
 let FILECOUNT=0;
 let TRASH=document.getElementById("message").lastElementChild;
